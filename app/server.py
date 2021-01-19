38430d97cdec2179db9a9ccc8013bfd34b742b88
Mahmoud Ashraf Mahmoud <38630967+uptotec@users.noreply.github.com>
2021-01-19 03:09:04 +0200

    add resize

--- app/server.py
+++ app/server.py
@@ -41,9 +41,11 @@ loop.close()
 async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-
+    img = Image.open(BytesIO(img_bytes))
+    new_image = img.resize((244, 244))
+    new_image.save('tmp/1234.png')
+    img2 = open_image('tmp/1234.png')
+    prediction = learn.predict(img2)[0]
     return JSONResponse({'result': str(prediction)})
 
 
